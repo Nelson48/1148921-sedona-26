002d14fe63a1b85d48347fcb180d51bd3261e5f1
Nelson48 <54740306+Nelson48@users.noreply.github.com>
2019-10-29 19:58:26 +0300

    fix

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,1 +1,22 @@
-var linkforma=document.querySelector(".search-form-title"),form=document.querySelector(".search-form");linkforma.addEventListener("click",function(a){a.preventDefault(),form.classList.add(".search-form-content")}),window.addEventListener("keydown",function(a){27===a.keyCode&&form.classList.contains("search-form-content")&&form.classList.remove("search-form-content")});
+var formDiv = document.querySelector('.search-form-content');
+// Читаем код, объявляю переменную formDiv (var formDiv), она будет являться (=) следующим. Я ищу в document селектор .search-form
+var formLink = document.querySelector('.search-form-title');
+// Тоже самое, ищу селектор в документа .search-form-title и засовываю в переменную formLink
+// Прикольно, что переменная formLink, но там селектор заголовка)
+formLink.addEventListener('click', function (evt) {
+  // Навешиваем какое-то событие на кнопку, в скобках говорим, что событие клик мыши
+  evt.preventDefault();
+  // Тут вообще пока не пытайся понять, кусок кода говорит типа, что сбрасываем все эффекты нажатия, которые могут быть на кнопке
+  formDiv.classList.toggle('vis-hidden');
+  // Тоглим класс нужный. При одном нажатии будет добавляться, при другом - появляться
+});
+
+formLink.addEventListener('keydown', function (evt) {
+  // Навешиваем какое-то событие на кнопку, в скобках говорим, что событие нажатие клавиши клавиатуры
+  if (evt.keyCode === 27) {
+    // Проверям, что есть кнопка клавиатуры - Enter, у неё свой код, 27
+    formDiv.classList.toggle('vis-hidden');
+    // Делаем тоже самое
+  }
+});
+
